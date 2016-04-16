411312cfb01fc4919ecae1dab8c34b68a0943488
ljr5102 <ljr5102@gmail.com>
2016-04-15 21:22:09 -0400

    Add some function for dom node collection

--- main.js
+++ main.js
@@ -4,6 +4,40 @@
     this.collection = array;
   };
 
+  DOMNodeCollection.prototype.each = function(cb) {
+    this.collection.forEach(function(el) {
+      cb(el);
+    });
+  };
+
+  DOMNodeCollection.prototype.html = function(html) {
+    if(html || html === "") {
+      this.each(function(item) {
+        item.innerHTML = html;
+      });
+    } else {
+      return this.collection[0].innerHTML;
+    }
+  };
+
+  DOMNodeCollection.prototype.empty = function() {
+    this.html("");
+    this.collection = [];
+  };
+
+  DOMNodeCollection.prototype.append = function(arg) {
+    if (arg instanceof DOMNodeCollection) {
+      arg.each(function(child) {
+        this.collection[0].appendChild(child);
+      }.bind(this));
+      // this.collection[0].appendChild(arg.collection[0]);
+    } else if (arg instanceof HTMLElement) {
+
+    } else if (typeof arg === "string") {
+
+    }
+  };
+
   window.$l = function(input) {
     if(input instanceof HTMLElement) {
       return new DOMNodeCollection([input]);
